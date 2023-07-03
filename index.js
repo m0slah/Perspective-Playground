40519a0473d6df94ecd1155ce9180a3452f4abb7
m0slah <muhammedslahqadir@gmail.com>
2023-07-03 09:41:08 +0300

    add return data

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-let vm = Vue.createApp({}).mount("#app");
+let vm = Vue.createApp({
+  data() {
+    return {
+      perspective: 100,
+      rotateX: 0,
+      rotateY: 0,
+      rorateZ: 0,
+    };
+  },
+  methods: {},
+  computed: {},
+}).mount("#app");
