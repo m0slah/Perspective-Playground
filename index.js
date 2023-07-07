19dde10c2fd4c4a4ce31f9345da6492cd39cc782
m0slah <muhammedslahqadir@gmail.com>
2023-07-08 01:43:53 +0300

    add Copy function

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
-let vm = Vue.createApp({
+Vue.createApp({
   data() {
     return {
       perspective: 100,
       rotateX: 0,
       rotateY: 0,
-      rorateZ: 0,
-    };
-  },
-  methods: {
-    Reset() {
-      this.perspective = 100;
-      this.rotateX = 0;
-      this.rotateY = 0;
-      this.rorateZ = 0;
-    },
+      rotateZ: 0
+    }
   },
   computed: {
     box() {
       return {
-        transform: `perspective(${this.perspective}px)
-            rotateX(${this.rotateX}deg)
-            rotateY(${this.rotateY}deg)
-            rotateZ(${this.rorateZ}deg)
-          `,
-      };
-    },
+        transform: `
+          perspective(${this.perspective}px)
+          rotateX(${this.rotateX}deg)
+          rotateY(${this.rotateY}deg)
+          rotateZ(${this.rotateZ}deg)
+        `
+      }
+    }
   },
-}).mount("#app");
+  methods: {
+    Reset() {
+      this.perspective = 100
+      this.rotateX = 0
+      this.rotateY = 0
+      this.rotateZ = 0
+    },
+    async Copy() {
+      let text = `transform:${this.box.transform};`
+      await navigator.clipboard.writeText(text)
+
+      alert("CSS Copied to Clipboard!")
+    }
+  }
+}).mount('#app')
